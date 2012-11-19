29c2e160517f99128f07e8d60844c11d3720ef6e
Gordon.Huang <gxing.huang@gmail.com>
2012-11-19 17:08:58 +0800

    rename spring configuration files of helloworld and quickstart project, fix maven archetype error.

--- examples/quickstart/src/test/java/com/androidwhy/examples/quickstart/repository/JpaMappingTest.java
+++ examples/quickstart/src/test/java/com/androidwhy/examples/quickstart/repository/JpaMappingTest.java
@@ -13,7 +13,7 @@ import javax.persistence.metamodel.Metamodel;
 
 import static org.junit.Assert.assertTrue;
 
-@ContextConfiguration(locations = { "/applicationContext.xml" })
+@ContextConfiguration(locations = {"/applicationContext-a.xml"})
 public class JpaMappingTest extends SpringTransactionalTestCase {
 
 	private static Logger logger = LoggerFactory.getLogger(JpaMappingTest.class);
--- examples/quickstart/src/test/java/com/androidwhy/examples/quickstart/repository/TaskDaoTest.java
+++ examples/quickstart/src/test/java/com/androidwhy/examples/quickstart/repository/TaskDaoTest.java
@@ -7,12 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 
 import static org.junit.Assert.assertEquals;
 
-@ContextConfiguration(locations = { "/applicationContext.xml" })
+@ContextConfiguration(locations = {"/applicationContext-a.xml"})
 public class TaskDaoTest extends SpringTransactionalTestCase {
 
 	@Autowired
